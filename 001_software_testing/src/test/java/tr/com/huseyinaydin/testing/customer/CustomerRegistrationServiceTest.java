9cfd5279987c4f22607dd5dcf93e076999c2ba14
huseyinaydin99 <huseyinaydin99@gmail.com>
2024-10-09 14:46:56 +0300

    Müşteri kaydı testi için capture kullanıldı.
    CustomerRegistrationService için capture ile customerRepository'e girilen arguman yakalanıp beklenen ile eşit mi diye test edildi.

--- 001_software_testing/src/test/java/tr/com/huseyinaydin/testing/customer/CustomerRegistrationServiceTest.java
+++ 001_software_testing/src/test/java/tr/com/huseyinaydin/testing/customer/CustomerRegistrationServiceTest.java
@@ -24,10 +24,50 @@ class CustomerRegistrationServiceTest {
     private CustomerRepository customerRepository;
 
     private CustomerRegistrationService underTest;
+	
+	@Captor
+    private ArgumentCaptor<Customer> customerArgumentCaptor;
+    /*
+        ArgumentCaptor, bir Mockito testi sırasında, bir metodun çağrıldığı argümanı yakalamak ve bu argüman 
+		üzerinde doğrulama yapmak için kullanılan bir araçtır, böylece testlerde daha ayrıntılı kontrol ve analiz sağlar.
+    */
 
     @BeforeEach
     void setUp() {
         MockitoAnnotations.initMocks(this);
         underTest = new CustomerRegistrationService(customerRepository);
+    }
+	
+	@Test
+    void itShouldSaveNewCustomer() {
+        // Given - bizim beklediğimiz değer veya referans.
+        String phoneNumber = "000099";
+        Customer customer = new Customer(UUID.randomUUID(), "Huseyin", phoneNumber);
+
+        // ... müşteri nesnesini tutan kaydolma isteği.
+        CustomerRegistrationRequest request = new CustomerRegistrationRequest(customer);
+
+        // ... DB'de '000099' tel nolu müşteri var mı? Beklenen referans Optional boş.
+        given(customerRepository.selectCustomerByPhoneNumber(phoneNumber))
+                .willReturn(Optional.empty());
+
+        //... Beklenen değer true mu? Telefon numarası doğrulama tel no doğru formatta mı?
+        given(phoneNumberValidator.test(phoneNumber)).willReturn(true);
+
+        // Müşteri kaydı.
+        underTest.registerNewCustomer(request);
+
+        // Then
+        then(customerRepository).should().save(customerArgumentCaptor.capture());
+        /*
+         Daha önce yapılan bir çağrıda kullanılan nesneyi yakalar.
+		 Bu, test sırasında save metoduna hangi nesnenin geçtiğini öğrenmek
+		 ve bu nesne üzerinde test yapmak için kullanılır.
+		 Böylece çağrılan nesnenin durumu ya da içeriği üzerinde daha ayrıntılı doğrulamalar yapabilirim.
+         */
+
+        //Müşteri argümanı yakalanan değeri.
+        Customer customerArgumentCaptorValue = customerArgumentCaptor.getValue();
+        assertThat(customerArgumentCaptorValue).isEqualTo(customer); //Yakalanan referans beklenen'e eşit mi?
     }
 }
