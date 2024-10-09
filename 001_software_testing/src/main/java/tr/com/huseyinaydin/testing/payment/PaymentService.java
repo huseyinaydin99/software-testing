63d42419974a401ae8b311f55c6fbd778a21c7bb
huseyinaydin99 <huseyinaydin99@gmail.com>
2024-10-09 16:45:10 +0300

    Ödeme işlemleri PaymentService'de yapıldı.
    1. Müşteri database'de yoksa hata fırlat.
    2. Parametreden gelen paymentRequest'in ödemesi desteklenenler arasında yoksa istisna fırlat.
    3. Ödeme bilgilerini al.
    4. Kart borcu varsa istisna fırlat.
    5. Ödemeyi kaydet.
    6. TODO: SMS gönderme işlemleri yapılacak.

--- 001_software_testing/src/main/java/tr/com/huseyinaydin/testing/payment/PaymentService.java
+++ 001_software_testing/src/main/java/tr/com/huseyinaydin/testing/payment/PaymentService.java
@@ -10,17 +10,56 @@ import java.util.UUID;
 @Service
 public class PaymentService {
 
+    private static final List<Currency> ACCEPTED_CURRENCIES = List.of(Currency.TL, Currency.GBP);
+
     private final CustomerRepository customerRepository;
     private final PaymentRepository paymentRepository;
+    private final CardPaymentCharger cardPaymentCharger;
 
     @Autowired
     public PaymentService(CustomerRepository customerRepository,
-                          PaymentRepository paymentRepository) {
+                          PaymentRepository paymentRepository,
+                          CardPaymentCharger cardPaymentCharger) {
         this.customerRepository = customerRepository;
         this.paymentRepository = paymentRepository;
+        this.cardPaymentCharger = cardPaymentCharger;
     }
 
     void chargeCard(UUID customerId, PaymentRequest paymentRequest) {
+        // 1. Müşteri database'de yoksa hata fırlat.
+        boolean isCustomerFound = customerRepository.findById(customerId).isPresent();
+        if (!isCustomerFound) {
+            throw new IllegalStateException(String.format("[%s] Id'li müşteri bulunamadı.", customerId));
+        }
 
+        // 2. Parametreden gelen paymentRequest'in ödemesi desteklenenler arasında yoksa istisna fırlat.
+        boolean isCurrencySupported = ACCEPTED_CURRENCIES.contains(paymentRequest.getPayment().getCurrency());
+
+        if (!isCurrencySupported) {
+            String message = String.format(
+                    "[%s] para birimi desteklenmiyor.",
+                    paymentRequest.getPayment().getCurrency());
+            throw new IllegalStateException(message);
+        }
+
+        // 3. Ödeme bilgilerini al.
+        CardPaymentCharge cardPaymentCharge = cardPaymentCharger.chargeCard(
+                paymentRequest.getPayment().getSource(),
+                paymentRequest.getPayment().getAmount(),
+                paymentRequest.getPayment().getCurrency(),
+                paymentRequest.getPayment().getDescription()
+        );
+
+        // 4. Kart borcu varsa istisna fırlat.
+        if (!cardPaymentCharge.isCardDebited()) {
+            throw new IllegalStateException(String.format("%s Id'li kart borçlu.", customerId));
+        }
+
+        // 5. Ödemeyi kaydet.
+        paymentRequest.getPayment().setCustomerId(customerId);
+
+        paymentRepository.save(paymentRequest.getPayment());
+
+        // 6. TODO: SMS gönderme işlemleri yapıalcak.
     }
 }
