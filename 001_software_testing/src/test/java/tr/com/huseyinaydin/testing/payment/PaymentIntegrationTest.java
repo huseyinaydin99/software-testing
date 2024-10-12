fd4c192ab905fb3cb29e1984c8d53a0f22d8ec6e
huseyinaydin99 <huseyinaydin99@gmail.com>
2024-10-12 16:36:29 +0300

    Ödeme işlemi başarılı bir şekilde kaydoluyor mu?
    Ödeme entegrasyon testi PaymentIntegrationTest sınıfında yapıldı.
    
    // Given - hazırlanan müşteri nesnesi.
    UUID customerId = UUID.randomUUID();
    Customer customer = new Customer(customerId, "Huseyin", "+90554554454");
    
    CustomerRegistrationRequest customerRegistrationRequest = new CustomerRegistrationRequest(customer);
    
    // Register işlemi / kaydolma.
    ResultActions customerRegResultActions = mockMvc.perform(put("/api/v1/customer-registration")
                    .contentType(MediaType.APPLICATION_JSON)
         .content(Objects.requireNonNull(objectToJson(customerRegistrationRequest))));
    
    // ... ödeme nesnesi.
    long paymentId = 1L;
    
    Payment payment = new Payment(paymentId, customerId, new BigDecimal("321.12"),
    Currency.TL, "x1x2x3x4", "Zekat/Fitre/Sadaka/Başımızın gözümüzün üstüne.");
    
    // ... ödeme nesnesini sarmalayan ödeme isteği nesnesi.
    PaymentRequest paymentRequest = new PaymentRequest(payment);
    
    // ... ödeme işlemini kaydet. [POST]
    ResultActions paymentResultActions = mockMvc.perform(post("/api/v1/payment")
                    .contentType(MediaType.APPLICATION_JSON)             .content(Objects.requireNonNull(objectToJson(paymentRequest))));
    
    // müşteri kaydolması ve ödeme kaydı durum kodu beklendisi 200 ok'dir. hal böyleyken test geçerli olur.
            customerRegResultActions.andExpect(status().isOk());
    paymentResultActions.andExpect(status().isOk());
    
    // DB'deki ödeme kaydı bizim beklediğimiz kayda eşit mi?
    // TODO: Payment repository kullanılmamalı bunun yerine yeni endpoint oluşturulmalı.
            assertThat(paymentRepository.findById(paymentId))
    .isPresent()
    .hasValueSatisfying(p -> assertThat(p).isEqualToComparingFieldByField(payment));
    
    // TODO: SMS işlemleri yapılabilir. Hani ödeme için yapılan onay SMS'i gibi.

--- 001_software_testing/src/test/java/tr/com/huseyinaydin/testing/payment/PaymentIntegrationTest.java
+++ 001_software_testing/src/test/java/tr/com/huseyinaydin/testing/payment/PaymentIntegrationTest.java
@@ -25,9 +25,56 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @SpringBootTest
 class PaymentIntegrationTest {
 
+    @Autowired
+    private PaymentRepository paymentRepository;
+
+    @Autowired
+    private MockMvc mockMvc;
+
     @Test
     void itShouldCreatePaymentSuccessfully() throws Exception {
+        // Given - hazırlanan müşteri nesnesi.
+        UUID customerId = UUID.randomUUID();
+        Customer customer = new Customer(customerId, "Huseyin", "+90554554454");
 
+        CustomerRegistrationRequest customerRegistrationRequest = new CustomerRegistrationRequest(customer);
+
+        // Register işlemi / kaydolma.
+        ResultActions customerRegResultActions = mockMvc.perform(put("/api/v1/customer-registration")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(Objects.requireNonNull(objectToJson(customerRegistrationRequest))));
+
+        // ... ödeme nesnesi.
+        long paymentId = 1L;
+
+        Payment payment = new Payment(
+                paymentId,
+                customerId,
+                new BigDecimal("321.12"),
+                Currency.TL,
+                "x1x2x3x4",
+                "Zekat/Fitre/Sadaka/Başımızın gözümüzün üstüne."
+        );
+
+        // ... ödeme nesnesini sarmalayan ödeme isteği nesnesi.
+        PaymentRequest paymentRequest = new PaymentRequest(payment);
+
+        // ... ödeme işlemini kaydet. [POST]
+        ResultActions paymentResultActions = mockMvc.perform(post("/api/v1/payment")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(Objects.requireNonNull(objectToJson(paymentRequest))));
+
+        // müşteri kaydolması ve ödeme kaydı durum kodu beklendisi 200 ok'dir. hal böyleyken test geçerli olur.
+        customerRegResultActions.andExpect(status().isOk());
+        paymentResultActions.andExpect(status().isOk());
+
+        // DB'deki ödeme kaydı bizim beklediğimiz kayda eşit mi?
+        // TODO: Payment repository kullanılmamalı bunun yerine yeni endpoint oluşturulmalı.
+        assertThat(paymentRepository.findById(paymentId))
+                .isPresent()
+                .hasValueSatisfying(p -> assertThat(p).isEqualToComparingFieldByField(payment));
+
+        // TODO: SMS işlemleri yapılabilir. Hani ödeme için yapılan onay SMS'i gibi.
     }
 
     private String objectToJson(Object object) {
